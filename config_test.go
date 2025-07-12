00304a63f1e6d5a22bf7c402e2827b360ecf9db3
Michael <michael@rothwell.us>
2025-07-12 16:48:41 +0200

    add better subscriber test

--- config_test.go
+++ config_test.go
@@ -1,12 +1,14 @@
 package configloader
 
 import (
+	"log"
+	"os"
 	"testing"
+	"time"
 )
 
 type TestConf struct {
 	Foo string
-	Bar string
 }
 
 func TestLoadConfig(t *testing.T) {
@@ -22,9 +24,6 @@ func TestLoadConfig(t *testing.T) {
 
 	if conf.Foo != "foo!" {
 		t.Errorf("expected 'foo' = 'foo!', got %q", conf.Foo)
-	}
-	if conf.Bar != "bar!" {
-		t.Errorf("expected 'bar' = 'bar!', got %q", conf.Bar)
 	}
 }
 
@@ -44,7 +43,56 @@ func TestSubscribeConfig(t *testing.T) {
 	if conf.Foo != "foo!" {
 		t.Errorf("expected 'foo' = 'foo!', got %q", conf.Foo)
 	}
-	if conf.Bar != "bar!" {
-		t.Errorf("expected 'bar' = 'bar!', got %q", conf.Bar)
+}
+
+func TestSubscribeConfigWithTempFile(t *testing.T) {
+	tmpfile, err := os.CreateTemp("", "config-*.yaml")
+	if err != nil {
+		t.Fatalf("failed to create temp file: %v", err)
 	}
+	defer os.Remove(tmpfile.Name())
+
+	initial := []byte("foo: foo!\n")
+	if _, err := tmpfile.Write(initial); err != nil {
+		t.Fatalf("failed to write initial config: %v", err)
+	}
+	tmpfile.Close()
+
+	loader, err := NewConfigLoader[TestConf](tmpfile.Name())
+	if err != nil {
+		t.Fatalf("error loading config: %v", err)
+	}
+	subscription := loader.Subscribe()
+
+	ticker := time.NewTicker(100 * time.Millisecond)
+	defer ticker.Stop()
+	count := 0
+	for {
+		count++
+		select {
+		case conf := <-subscription:
+			log.Printf("new config, Foo=%q", conf.Foo)
+			if count >= 2 && conf.Foo == "newfoo!" {
+				return
+			}
+			if count == 1 && conf.Foo != "foo!" {
+				t.Errorf("expected 'foo' = 'newfoo!', got %q", conf.Foo)
+			}
+			if count == 2 && conf.Foo != "newfoo!" {
+				t.Errorf("expected 'foo' = 'newfoo!', got %q", conf.Foo)
+			}
+		case <-ticker.C:
+			// Update config file
+			updated := []byte("foo: newfoo!\n")
+			if err := os.WriteFile(tmpfile.Name(), updated, 0644); err != nil {
+				t.Fatalf("failed to update config: %v", err)
+			}
+
+		}
+		if count > 5 {
+			log.Printf("exiting after 5 iterations")
+			return
+		}
+	}
+
 }
